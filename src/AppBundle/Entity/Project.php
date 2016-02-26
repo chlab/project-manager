2de33ac192450ae0f4bf0db90b5d3e8f6e4bfe59
Christof Leuenberger <chris@chlab.ch>
2016-02-26 08:41:39 +0100

    updated schema

--- src/AppBundle/Entity/Project.php
+++ src/AppBundle/Entity/Project.php
@@ -60,27 +60,6 @@ class Project
     private $title;
 
     /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="created", type="datetime", nullable=true)
-     */
-    private $created;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="modified", type="datetime", nullable=true)
-     */
-    private $modified;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="deleted", type="datetime", nullable=true)
-     */
-    private $deleted;
-
-    /**
      * @var integer
      *
      * @ORM\Column(name="priority", type="smallint", nullable=true)
@@ -276,78 +255,6 @@ class Project
     public function getTitle()
     {
         return $this->title;
-    }
-
-    /**
-     * Set created
-     *
-     * @param \DateTime $created
-     *
-     * @return Project
-     */
-    public function setCreated($created)
-    {
-        $this->created = $created;
-
-        return $this;
-    }
-
-    /**
-     * Get created
-     *
-     * @return \DateTime
-     */
-    public function getCreated()
-    {
-        return $this->created;
-    }
-
-    /**
-     * Set modified
-     *
-     * @param \DateTime $modified
-     *
-     * @return Project
-     */
-    public function setModified($modified)
-    {
-        $this->modified = $modified;
-
-        return $this;
-    }
-
-    /**
-     * Get modified
-     *
-     * @return \DateTime
-     */
-    public function getModified()
-    {
-        return $this->modified;
-    }
-
-    /**
-     * Set deleted
-     *
-     * @param \DateTime $deleted
-     *
-     * @return Project
-     */
-    public function setDeleted($deleted)
-    {
-        $this->deleted = $deleted;
-
-        return $this;
-    }
-
-    /**
-     * Get deleted
-     *
-     * @return \DateTime
-     */
-    public function getDeleted()
-    {
-        return $this->deleted;
     }
 
     /**
@@ -668,6 +575,11 @@ class Project
         $remainingDays = $this->getRemainingDays();
         $diff = $this->getEndDate()->diff($this->getStartDate());
         $totalDays = $diff->format('%d');
-        return 100/$totalDays*$remainingDays;
+        if ($totalDays > 0) {
+            return 100/$totalDays*$remainingDays;    
+        }
+        else {
+            return 0;
+        }
     }
 }
